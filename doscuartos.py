e57722816b7407a83797131315291f878e73d296
lcontiveros <luiscarlosov22@gmail.com>
2015-02-04 17:52:57 -0700

    Problema 2

--- doscuartos.py
+++ doscuartos.py
@@ -37,17 +37,17 @@ class DosCuartos(entornos.Entorno):
         if not self.accion_legal(estado, accion):
             raise ValueError("La accion no es legal para este estado")
 
-        robot, A, B = estado
+        robot,A,B= estado
 
         return (('A', A, B) if accion == 'irA' else
                 ('B', A, B) if accion == 'irB' else
                 (robot, A, B) if accion == 'noOp' else
                 ('A', 'limpio', B) if accion == 'limpiar' and robot == 'A' else
                 ('B', A, 'limpio'))
-
+#El agente solo regresa el cuarto en el que esta
     def sensores(self, estado):
-        robot, A, B = estado
-        return robot, A if robot == 'A' else B
+        robot,A,B= estado
+        return robot
 
     def accion_legal(self, estado, accion):
         return accion in ('irA', 'irB', 'limpiar', 'noOp')
@@ -56,7 +56,37 @@ class DosCuartos(entornos.Entorno):
         robot, A, B = estado
         return 0 if accion == 'noOp' and A == B == 'limpio' else -1
 
+class AgenteReactivoModeloDosCuartos(entornos.Agente):
+    """
+    Un agente reactivo basado en modelo
 
+    """
+    def __init__(self):
+        """
+        Consideramos los dos cuartos sucios en principio al no conocer la situacion actual
+        """
+        self.modelo = ['A', 'sucio', 'sucio']
+        self.lugar = {'A': 1, 'B': 2}
+
+    def programa(self, percepcion):
+        robot = percepcion
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+
+
+        # Decide sobre el modelo interno
+        A, B = self.modelo[1], self.modelo[2]
+        if (A == B == 'limpio'):
+            return('noOp')
+        elif (self.modelo[self.lugar[robot]] == 'sucio'): #revisamos si ya limpiamos este cuarto
+                self.modelo[self.lugar[robot]] = 'limpio' #Guardamos en el modelo interno que ya se limpio este cuarto y limpiamos
+                return('limpiar')
+        elif (robot == 'B'):
+                return('irA')
+        else:
+                return('irB')
+#El desempeño del agente racional con modelo fue de -3 y el del agente aleatorio de -85
 class AgenteAleatorio(entornos.Agente):
     """
     Un agente que solo regresa una accion al azar entre las acciones legales
@@ -67,68 +97,19 @@ class AgenteAleatorio(entornos.Agente):
 
     def programa(self, percepcion):
         return choice(self.acciones)
-
-
-class AgenteReactivoDoscuartos(entornos.Agente):
-    """
-    Un agente reactivo simple
-
-    """
-
-    def programa(self, percepcion):
-        robot, situacion = percepcion
-        return ('limpiar' if situacion == 'sucio' else
-                'irA' if robot == 'B' else
-                'irB')
-
-
-class AgenteReactivoModeloDosCuartos(entornos.Agente):
-    """
-    Un agente reactivo basado en modelo
-
-    """
-    def __init__(self):
-        """
-        Inicializa el modelo interno en el peor de los casos
-
-        """
-        self.modelo = ['A', 'sucio', 'sucio']
-        self.lugar = {'A': 1, 'B': 2}
-
-    def programa(self, percepcion):
-        robot, situacion = percepcion
-
-        # Actualiza el modelo interno
-        self.modelo[0] = robot
-        self.modelo[self.lugar[robot]] = situacion
-
-        # Decide sobre el modelo interno
-        A, B = self.modelo[1], self.modelo[2]
-        return ('noOp' if A == B == 'limpio' else
-                'limpiar' if situacion == 'sucio' else
-                'irA' if robot == 'B' else
-                'irB')
-
-
 def test():
     """
     Prueba del entorno y los agentes
 
     """
-    print "Prueba del entorno de dos cuartos con un agente aleatorio"
+    print "Prueba del entorno de tres cuartos con un agente aleatorio"
     entornos.simulador(DosCuartos(),
                        AgenteAleatorio(['irA', 'irB', 'limpiar', 'noOp']),
-                       ('A', 'sucio', 'sucio'), 100)
-
-    print "Prueba del entorno de dos cuartos con un agente reactivo"
-    entornos.simulador(DosCuartos(),
-                       AgenteReactivoDoscuartos(),
-                       ('A', 'sucio', 'sucio'), 100)
-
+                       ('A','sucio','sucio'), 100)
     print "Prueba del entorno de dos cuartos con un agente reactivo"
     entornos.simulador(DosCuartos(),
                        AgenteReactivoModeloDosCuartos(),
-                       ('A', 'sucio', 'sucio'), 100)
+                       ('A','sucio','sucio'), 100)
 
 if __name__ == '__main__':
     test()
