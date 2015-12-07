9f08fa30e9a9a3d670cf403180a8b29d095d2635
Christian Ruink <cruink@NSL-CRUINKs-MacBook-Pro.local>
2015-12-06 23:40:13 -0700

    Tarea 01 listo.

--- tarea_1.py
+++ tarea_1.py
@@ -44,9 +44,236 @@ la tarea.
 
 
 """
-__author__ = 'escribe_tu_nombre'
+__author__ = 'Christian Ruink'
 
 import entornos
+import random
 # Requiere el modulo entornos.py
 # El modulo doscuartos.py puede ser de utilidad para reutilizar código
 # Agrega los modulos que requieras de python
+from random import choice
+
+
+class tareaCuartos(entornos.Entorno):
+    """
+    Clase para un entorno de dos cuartos. Muy sencilla solo regrupa métodos.
+
+    El estado se define como (robot, A, B) 
+    donde robot puede tener los valores "A", "B"
+    A y B pueden tener los valores "limpio", "sucio"
+
+    Las acciones válidas en el entorno son 
+            "irA", "irB", "limpiar" y "noOp". 
+    Todas las acciones son válidas en todos los estados.
+
+    Los sensores es una tupla 
+                (robot, limpio?) 
+    con la ubicación del robot y el estado de limieza
+    
+    `A = {"irDerecha", "irIzquierda", "subir", "bajar", "limpiar" y "noOp"}`
+
+    """
+
+    def transicion(self, estado, accion):
+        print "ACCION: " + accion
+        if not self.accion_legal(estado, accion):
+            raise ValueError("La accion no es legal para este estado")
+        
+        print (estado)
+
+        robot, A1, B1, C1, A2, B2 = estado
+        if (robot == 'A1' or robot  == 'B1' or robot == 'C1'):
+            if robot == 'A1':
+                return (('B1', A1, B1, C1, A2, B2) if accion == 'irDerecha' else
+                        ('A2', A1, B1, C1, A2, B2) if accion == 'subir' else 
+                        ('A1', 'limpio', B1, C1, A2, B2) if accion == 'limpiar' else
+                        ('A1', A1, B1, C1, A2, B2))
+            if robot == 'B1':
+                return (('C1', A1, B1, C1, A2, B2) if accion == 'irDerecha' else
+                        ('A1', A1, B1, C1, A2, B2) if accion == 'irIzquierda' else
+                        ('B2', A1, B1, C1, A2, B2) if accion == 'subir' else 
+                        ('B1', A1, 'limpio', C1, A2, B2) if accion == 'limpiar' else
+                        ('B1', A1, B1, C1, A2, B2))
+            if robot == 'C1':
+                return (('B1', A1, B1, C1, A2, B2) if accion == 'irIzquierda' else
+                        ('A1', A1, B1, C1, A2, B2) if accion == 'subir' else 
+                        ('C1', A1, B1, 'limpio', A2, B2) if accion == 'limpiar' else
+                        ('A1', A1, B1, C1, A2, B2))
+        else:
+            if robot == 'A2':
+                return (('B2', A1, B1, C1, A2, B2) if accion == 'irDerecha' else
+                        ('A1', A1, B1, C1, A2, B2) if accion == 'bajar' else 
+                        ('A2', A1, B1, C1, 'limpio', B2) if accion == 'limpiar' else
+                        ('A2', A1, B1, C1, A2, B2))
+            if robot == 'B2':
+                return (('A2', A1, B1, C1, A2, B2) if accion == 'irIzquierda' else
+                        ('B1', A1, B1, C1, A2, B2) if accion == 'bajar' else 
+                        ('B2', A1, B1, C1, 'limpio', B2) if accion == 'limpiar' else
+                        ('B2', A1, B1, C1, A2, B2))
+
+
+    def sensores(self, estado):
+        robot, A1, B1, C1, A2, B2 = estado
+        if robot == 'A1': return robot, A1
+        if robot == 'B1': return robot, B1
+        if robot == 'C1': return robot, C1
+        if robot == 'A2': return robot, A2
+        if robot == 'B2': return robot, B2
+
+            
+        
+    def accion_legal(self, estado, accion):
+        return accion in ('irDerecha', 'irIzquierda', 'subir', 'bajar', 'limpiar', 'noOp')
+
+    def desempeno_local(self, estado, accion):
+        robot, A1, B1, C1, A2, B2 = estado
+        if (accion == 'noOp' and A1 == B1 == C1== A2== B2== 'limpio'): return 0
+        if (accion == 'subir' or accion == 'bajar'): return -2 
+        return -1
+
+
+class AgenteAleatorio(entornos.Agente):
+    """
+    Un agente que solo regresa una accion al azar entre las acciones legales
+
+    """
+    def __init__(self, acciones):
+        self.acciones = acciones
+
+    def programa(self, percepcion):
+        return choice(self.acciones)
+
+
+class AgenteReactivoDoscuartos(entornos.Agente):
+    """
+    Un agente reactivo simple
+
+    """
+
+    def programa(self, percepcion):
+        robot, situacion = percepcion
+        return ('limpiar' if situacion == 'sucio' else
+                'irA' if robot == 'B' else
+                'irB')
+    
+class AgenteReactivoTarea(entornos.Agente):
+    """
+    Un agente reactivo simple
+
+    """
+
+    def programa(self, percepcion):
+        robot, situacion = percepcion
+        return ('limpiar' if situacion == 'sucio' else
+                'irDerecha' if (robot == 'A1' or robot=='A2') else
+                'irIzquierda')
+
+
+
+class AgenteReactivoModeloDosCuartos(entornos.Agente):
+    """
+    Un agente reactivo basado en modelo
+
+    """
+    def __init__(self):
+        """
+        Inicializa el modelo interno en el peor de los casos
+
+        """
+        self.modelo = ['A', 'sucio', 'sucio']
+        self.lugar = {'A': 1, 'B': 2}
+
+    def programa(self, percepcion):
+        robot, situacion = percepcion
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[self.lugar[robot]] = situacion
+
+        # Decide sobre el modelo interno
+        A, B = self.modelo[1], self.modelo[2]
+        return ('noOp' if A == B == 'limpio' else
+                'limpiar' if situacion == 'sucio' else
+                'irA' if robot == 'B' else
+                'irB')
+
+class AgenteRacionalModeloDosCuartos(entornos.Agente):
+    """
+    Un agente que limpie 80% del tiempo.
+    """
+    def __init__(self):
+        """
+        Inicializa el modelo interno en el peor de los casos
+
+        """
+        self.modelo = ['A1', 'sucio', 'sucio', 'sucio', 'sucio', 'sucio']
+        self.lugar = {'A1': 1, 'B1': 2, 'C1': 3, 'A2': 4, 'B2': 5 }
+
+    def programa(self, percepcion):
+        robot, situacion = percepcion
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[self.lugar[robot]] = situacion
+
+        # Decide sobre el modelo interno
+        A, B = self.modelo[1], self.modelo[2]
+        limpia = (random.random() > .5 )
+        
+        return ('noOp' if A == B == 'limpio' else
+                'limpiar' if situacion == 'sucio' else
+                'irA' if robot == 'B' else
+                'irB')
+    
+class AgenteReactivoModeloTarea(entornos.Agente):
+    """
+    Un agente reactivo basado en modelo
+
+    """
+    def __init__(self):
+        """
+        Inicializa el modelo interno en el peor de los casos
+
+        """
+        self.modelo = ['A1', 'sucio', 'sucio', 'sucio', 'sucio', 'sucio']
+        self.lugar = {'A1': 1, 'B1': 2, 'C1': 3, 'A2': 4, 'B2': 5 }
+
+
+    def programa(self, percepcion):
+        robot, situacion = percepcion
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[self.lugar[robot]] = situacion
+
+        # Decide sobre el modelo interno
+        A, B = self.modelo[1], self.modelo[2]
+        return ('noOp' if A == B == 'limpio' else
+                'limpiar' if situacion == 'sucio' else
+                'irA' if robot == 'B' else
+                'irB')
+
+
+def test():
+    """
+    Prueba del entorno y los agentes
+
+    """
+    print "Prueba del entorno de tarea con un agente aleatorio"
+    entornos.simulador(tareaCuartos(),
+                       AgenteAleatorio(['irDerecha', 'irIzquierda', 'subir', 'bajar', 'limpiar', 'noOp']),
+                       ('A1', 'sucio', 'sucio', 'sucio', 'sucio', 'sucio'), 100)
+
+    print "Prueba del entorno de tarea con un agente reactivo"
+    entornos.simulador(tareaCuartos(),
+                       AgenteReactivoTarea(),
+                       ('A1', 'sucio', 'sucio', 'sucio', 'sucio', 'sucio'), 100)
+
+    print "Prueba del entorno de dos pisos con un agente racional"
+    entornos.simulador(tareaCuartos(),
+                       AgenteRacionalModeloDosCuartos(),
+                       ('A1', 'sucio', 'sucio', 'sucio', 'sucio', 'sucio'), 100)
+
+if __name__ == '__main__':
+    test()
+
