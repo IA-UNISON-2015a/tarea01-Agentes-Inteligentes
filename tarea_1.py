499d8a14f046f642acfc4186c802167942d4114e
Rafael Castillo <savoytruffle321@gmail.com>
2017-08-29 01:24:19 -0700

    Quitar lo de arriba (nomas pa que haya menos lineas la verdad)

--- tarea_1.py
+++ tarea_1.py
@@ -1,56 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-tarea_1.py
-------------
 
-Tarea de desarrollo de entornos y agentes
-==========================================
-
-En esta tarea realiza las siguiente acciones:
-
-1.- Desarrolla un entorno similar al de los dos cuartos (el cual se encuentra
-    en el módulo doscuartos_o.py), pero con tres cuartos en el primer piso,
-    y tres cuartos en el segundo piso.
-
-    El entorno se llamará SeisCuartos
-
-    Las acciones totales serán
-
-    ["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"]
-
-    La acción de "subir" solo es legal en el piso de abajo (cualquier cuarto),
-    y la acción de "bajar" solo es legal en el piso de arriba.
-
-    Las acciones de subir y bajar son mas costosas en término de energía
-    que ir a la derecha y a la izquierda, por lo que la función de desempeño
-    debe de ser de tener limpios todos los cuartos, con el menor numero de
-    acciones posibles, y minimozando subir y bajar en relación a ir a los
-    lados.
-
-2.- Diseña un Agente reactivo basado en modelo para este entorno y compara
-    su desempeño con un agente aleatorio despues de 100 pasos de simulación.
-
-3.- Al ejemplo original de los dos cuartos, modificalo de manera que el agente
-    solo pueda saber en que cuarto se encuentra pero no sabe si está limpio
-    o sucio.
-
-    A este nuevo entorno llamalo DosCuartosCiego
-
-    Diseña un agente racional para este problema, pruebalo y comparalo
-    con el agente aleatorio.
-
-4.- Reconsidera el problema original de los dos cuartos, pero ahora modificalo
-    para que cuando el agente decida aspirar, el 80% de las veces limpie pero
-    el 20% (aleatorio) deje sucio el cuarto. Diseña un agente racional
-    para este problema, pruebalo y comparalo con el agente aleatorio.
-
-    A este entorno llamalo DosCuartosEstocástico
-
-Todos los incisos tienen un valor de 25 puntos sobre la calificación de
-la tarea.
-
-"""
 __author__ = 'Rafael Alejandro Castillo Lopez'
 
 import random
@@ -342,7 +292,7 @@ class BlindTwoRoomReactiveModelAgent(TwoRoomReactiveModelAgent):
         self.cycle_state = 'clean'
 
     def program(self, position):
-        if self.starting_position != positiion and cycle_state == 'dirty':
+        if self.starting_position != position and self.cycle_state == 'dirty':
             return 'noop'
 
         self.cycle_state = 'clean' if self.cycle_state == 'dirty' else 'dirty'
@@ -398,7 +348,7 @@ if __name__ == '__main__':
     test_agent(agent, environment)
 
     # probar agente aleatorio a ciegas
-    environment = BlindHouseEnvironment()
+    environment = BlindTwoRoomEnvironment()
     agent = RandomAgent(environment)
     test_agent(agent, environment)
 
