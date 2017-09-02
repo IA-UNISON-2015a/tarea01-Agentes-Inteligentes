21fd6d54f04dcf60a852d259ee2f6371eff4f577
fernando Salcido Ruiz <fernando Salcido Ruiz>
2017-09-02 13:36:34 -0700

    solo resta hacer el agente racional del inciso 3

--- tarea_1.py
+++ tarea_1.py
@@ -54,7 +54,58 @@ la tarea.
 __author__ = 'escribe_tu_nombre'
 
 import entornos_o
-from random import choice
+from random import choice,random
+class DosCuartosCiego(entornos_o.Entorno):
+    """
+    Clase para un entorno de dos cuartos. Muy sencilla solo regrupa métodos.
+
+    El estado se define como (robot, A, B)
+    donde robot puede tener los valores "A", "B"
+    A y B pueden tener los valores "limpio", "sucio"
+
+    Las acciones válidas en el entorno son ("ir_A", "ir_B", "limpiar", "nada").
+    Todas las acciones son válidas en todos los estados.
+
+    Los sensores es una tupla (robot, limpio?)
+    con la ubicación del robot y el estado de limpieza
+
+    """
+    def __init__(self, x0=["A", "sucio", "sucio"]):
+        """
+        Por default inicialmente el robot está en A y los dos cuartos
+        están sucios
+
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        return acción in ("ir_A", "ir_B", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, a, b = self.x
+        if acción is not "nada" or a is "sucio" or b is "sucio":
+            self.desempeño -= 1
+        if acción is "limpiar":
+            self.limpiar(self.x[0])
+        elif acción is "ir_A":
+            self.x[0] = "A"
+        elif acción is "ir_B":
+            self.x[0] = "B"
+    def limpiar(self,target):
+        self.x[" AB".find(target)] = "limpio"
+    def percepción(self):
+        return self.x[0]
+class DosCuartosEstocastico(DosCuartosCiego):
+    def limpiar(self,target):
+        if random() < 0.5:
+            super().limpiar(target)
+    def percepción(self):
+        return self.x[0], self.x[" AB".find(self.x[0])]
+
 class SeisCuartos(entornos_o.Entorno):
     """
     Clase para un entorno de dos cuartos. Muy sencilla solo regrupa métodos.
@@ -195,13 +246,28 @@ class AgenteReactivoModeloSeisCuartos(entornos_o.Agente):
             return 'subir'
         '''
 
+class AgenteReactivoModeloDosCuartos(entornos_o.Agente):
+    def __init__(self):
+        self.modelo = ['A', 'sucio', 'sucio']
 
+    def programa(self, percepción):
+        robot, situación = percepción
+        print(percepción)
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[' AB'.find(robot)] = situación
+
+        # Decide sobre el modelo interno
+        a, b = self.modelo[1], self.modelo[2]
+        return ('nada' if a == b == 'limpio' else
+                'limpiar' if situación == 'sucio' else
+                'ir_A' if robot == 'B' else 'ir_B')
 
 def test():
     """
     Prueba del entorno y los agentes
 
-    """
+
     print("Prueba del entorno con un agente aleatorio")
     entornos_o.simulador(SeisCuartos(),
                          AgenteAleatorio(["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"]),
@@ -210,9 +276,14 @@ def test():
     print("Prueba del entorno con un agente reactivo")
     entornos_o.simulador(SeisCuartos(), AgenteReactivoModeloSeisCuartos(), 100)
 
-    #print("Prueba del entorno con un agente reactivo con modelo")
-    #entornos_o.simulador(DosCuartos(), AgenteReactivoModeloDosCuartos(), 100)
-
+    print("Prueba del entorno con un agente reactivo con modelo")
+    entornos_o.simulador(DosCuartos(), AgenteReactivoModeloDosCuartos(), 100)
+    entornos_o.simulador(DosCuartosEstocastico(),
+                         AgenteAleatorio(['ir_A', 'ir_B', 'limpiar', 'nada']),
+                         100)
+    """
+    print("Prueba del entorno con un agente reactivo con modelo")
+    entornos_o.simulador(DosCuartosEstocastico(), AgenteReactivoModeloDosCuartos(), 100)
 
 if __name__ == "__main__":
     test()
