29430b532fdcd8bfe0705d03d28b778a09f5c5d7
Giovanni <giovanni.lopez3523@gmail.com>
2017-09-02 23:04:32 -0700

    SixRooms, Finished?

--- tarea_1.py
+++ tarea_1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import entornos_o
+
+__author__ = 'Giovanni Lopez'
 """
 tarea_1.py
 ------------
@@ -27,10 +30,58 @@ En esta tarea realiza las siguiente acciones:
     debe de ser de tener limpios todos los cuartos, con el menor numero de
     acciones posibles, y minimozando subir y bajar en relación a ir a los
     lados.
+"""
+    class SixRooms(entornos_o.Area):
+        """
 
+        """
+        def __init__(self, x0 = ["A","dirty","dirty","dirty","dirty","dirty","dirty"]):
+            self.x = x0[:]
+            self.desempeño = 0
+
+        def legal_action(self,action):
+            return action in ("up","down","right","left","clean","nothing")
+
+        def transition(self,action):
+            raise ValueError("This is not a legal action to take, please try again")
+
+            robot, a, b, c, d, e, f = self.x
+            floor = "down"
+            if action in not "nothing" or a is "dirty" or b is "dirty" or c is "dirty" or d is "dirty" or e is "dirty" or f is "dirty":
+                self.desempeño -= 1
+            if action is "clean":
+                self.x[" ABCDEF".find(self.x[0])] = "limpio"
+            elif action is "right":
+                if self.x[0] is "C" or self.x[0] is "F":
+                    raise ValueError("This is not a legal action to take, please try again")
+                elif:
+                    self.x[0] = self.x[" ABCDEF".find(self.x[0])] + 1
+            elif action is "left":
+                if self.x[0] is "A" or self.x[0] is "D":
+                    raise ValueError("This is not a legal action to take, please try again")
+                elif:
+                    self.x[0] = self.x[" ABCDEF".find(self.x[0])] - 1
+            elif action is "up":
+                if floor is "up":
+                    raise ValueError("This is not a legal action to take, please try again")
+                elif:
+                    self.x[0] = self.x[" ABCDEF".find(self.x[0])] + 3
+            elif action is "down":
+                if floor is "down":
+                    raise ValueError("This is not a legal action to take, please try again")
+                elif:
+                    self.x[0] = self.x[" ABCDEF".find(self.x[0])] - 1
+
+        def perception(self):
+            return self.x[0], self.x[" ABCDEF".find(self.x[0])]
+
+
+"""
 2.- Diseña un Agente reactivo basado en modelo para este entorno y compara
     su desempeño con un agente aleatorio despues de 100 pasos de simulación.
+"""
 
+"""
 3.- Al ejemplo original de los dos cuartos, modificalo de manera que el agente
     solo pueda saber en que cuarto se encuentra pero no sabe si está limpio
     o sucio.
