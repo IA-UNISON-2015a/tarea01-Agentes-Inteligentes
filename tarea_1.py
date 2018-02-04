d47e4a1ce00e2d06add588ebc39a908377d87415
Giovanni <giovanni.lopez3523@gmail.com>
2018-02-03 19:14:04 -0700

    Primer enciso, voy consiguiendo que mi lap jale

--- tarea_1.py
+++ tarea_1.py
@@ -7,54 +7,140 @@ tarea_1.py
 Tarea de desarrollo de entornos y agentes
 ==========================================
 
-En esta tarea realiza las siguiente acciones:
+1. Desarrolla un entorno similar al de los dos cuartos (el cual se
+   encuentra en el módulo doscuartos_o.py), pero con tres cuartos en
+   el primer piso, y tres cuartos en el segundo piso.
+   
+   El entorno se llamará `SeisCuartos`.
 
-1.- Desarrolla un entorno similar al de los dos cuartos (el cual se encuentra
-    en el módulo doscuartos_o.py), pero con tres cuartos en el primer piso,
-    y tres cuartos en el segundo piso.
+   Las acciones totales serán
+   
+   ```
+   ["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"]
+   ``` 
+    
+   La acción de `"subir"` solo es legal en el piso de abajo, en los cuartos de los extremos, 
+   mientras que la acción de `"bajar"` solo es legal en el piso de arriba y en el cuarto de el centro (dos
+   escaleras para subir, una escalera para bajar).
 
-    El entorno se llamará SeisCuartos
+   Las acciones de subir y bajar son mas costosas en término de
+   energía que ir a la derecha y a la izquierda, por lo que la función
+   de desempeño debe de ser de tener limpios todos los cuartos, con el
+   menor numero de acciones posibles, y minimizando subir y bajar en
+   relación a ir a los lados. El costo de limpiar es menor a los costos
+   de cualquier acción.
 
-    Las acciones totales serán
+2. Diseña un Agente reactivo basado en modelo para este entorno y
+   compara su desempeño con un agente aleatorio despues de 100 pasos
+   de simulación.
 
-    ["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"]
+3. Al ejemplo original de los dos cuartos, modificalo de manera que el
+   agente solo pueda saber en que cuarto se encuentra pero no sabe si
+   está limpio o sucio.
 
-    La acción de "subir" solo es legal en el piso de abajo (cualquier cuarto),
-    y la acción de "bajar" solo es legal en el piso de arriba.
+   A este nuevo entorno llamalo `DosCuartosCiego`.
 
-    Las acciones de subir y bajar son mas costosas en término de energía
-    que ir a la derecha y a la izquierda, por lo que la función de desempeño
-    debe de ser de tener limpios todos los cuartos, con el menor numero de
-    acciones posibles, y minimozando subir y bajar en relación a ir a los
-    lados.
+   Diseña un agente racional para este problema, pruebalo y comparalo
+   con el agente aleatorio.
 
-2.- Diseña un Agente reactivo basado en modelo para este entorno y compara
-    su desempeño con un agente aleatorio despues de 100 pasos de simulación.
+4. Reconsidera el problema original de los dos cuartos, pero ahora
+   modificalo para que cuando el agente decida aspirar, el 80% de las
+   veces limpie pero el 20% (aleatorio) deje sucio el cuarto. Igualmente, 
+   cuando el agente decida cambiar de cuarto, se cambie correctamente de cuarto el 90% de la veces
+   y el 10% se queda en su lugar. Diseña
+   un agente racional para este problema, pruebalo y comparalo con el
+   agente aleatorio.
 
-3.- Al ejemplo original de los dos cuartos, modificalo de manera que el agente
-    solo pueda saber en que cuarto se encuentra pero no sabe si está limpio
-    o sucio.
-
-    A este nuevo entorno llamalo DosCuartosCiego
-
-    Diseña un agente racional para este problema, pruebalo y comparalo
-    con el agente aleatorio.
-
-4.- Reconsidera el problema original de los dos cuartos, pero ahora modificalo
-    para que cuando el agente decida aspirar, el 80% de las veces limpie pero
-    el 20% (aleatorio) deje sucio el cuarto. Diseña un agente racional
-    para este problema, pruebalo y comparalo con el agente aleatorio.
-
-    A este entorno llamalo DosCuartosEstocástico
+   A este entorno llámalo `DosCuartosEstocástico`.
 
 Todos los incisos tienen un valor de 25 puntos sobre la calificación de
 la tarea.
 
 """
-__author__ = 'escribe_tu_nombre'
+__author__ = 'Giovanni_Lopez_Celaya'
 
-import entorno_o
+import entornos_o
+from random import choice
 
+class SeisCuartos(entornos_o.Entorno):
+    """
+    Clase para un entorno de dos cuartos. Muy sencilla solo regrupa métodos.
+    El estado se define como (robot, A, B)
+    donde robot puede tener los valores "A", "B"
+    A y B pueden tener los valores "limpio", "sucio"
+    Las acciones válidas en el entorno son ("ir_A", "ir_B", "limpiar", "nada").
+    Todas las acciones son válidas en todos los estados.
+    Los sensores es una tupla (robot, limpio?)
+    con la ubicación del robot y el estado de limpieza (donde se encuentra el robot).
+    """
+
+    def __init__(self, x0=["A", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"]):
+        """ El robot esta en el cuarto A y todos los cuartos estan sucios
+            x=[robot,A,B,C,D,E,F]
+            cuartos: D,E,F
+                    A,B,C
+        Por default, el robot inicia en A y los dos cuartos están sucios
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        return acción in ("ir_Der", "ir_Izq", "subir", "bajar", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, a, b, c, d, e, f = self.x #Se asigna el lugar donde empezara el robot y las situaciones (sucio/limpio) de cada cuarto.
+
+        if acción != "nada" or a is "sucio" or b is "sucio" or c is"sucio" or d is "sucio" or e is "sucio" or f is "sucio":
+            if acción is "subir" or acción is "bajar":
+                self.desempeño -= 2
+            else:
+                self.desempeño -= 1
+
+        if acción is "limpiar":
+            self.x[" ABCDEF".find(self.x[0])] = "limpio"
+        # Posibles formas en que el robot se puede mover dependiendo de el cuarto en donde se encuentre.
+        elif acción is "ir_Izq" and self.x[0] is "B" or acción is "bajar" and self.x[0] is "D":
+            self.x[0] = "A"
+        elif acción is "ir_Der" and self.x[0] is "A" or acción is "bajar" and self.x[0] is "E" or acción is "ir_Izq" and self.x[0] is "C":
+            self.x[0] = "B"
+        elif acción is "ir_Der" and self.x[0] is "B" or acción is "bajar" and self.x[0] is "F":
+            self.x[0] = "C"
+        elif acción is "subir" and self.x[0] is "C" or acción is "ir_Der" and self.x[0] is "E":
+            self.x[0] = "F"
+        elif acción is "subir" and self.x[0] is "B" or acción is "ir_Izq" and self.x[0] is "F" or acción is "ir_Der" and self.x[0] is "D":
+            self.x[0] = "E"
+        elif acción is "subir" and self.x[0] is "A" or acción is "ir_Izq" and self.x[0] is "E":
+            self.x[0] = "D"
+
+    def percepción(self):
+        return self.x[0], self.x[" ABCDEF".find(self.x[0])]
+    
+class AgenteAleatorio(entornos_o.Agente):
+    """
+    Un agente que solo regresa una accion al azar entre las acciones legales
+    """
+
+    def __init__(self, acciones):
+        self.acciones = acciones
+
+    def programa(self, percepción):
+        return choice(self.acciones)
+    
+def test():
+    """
+    Prueba del entorno y los agentes
+    """
+    print("Prueba del entorno con un agente aleatorio en seis cuartos.")
+    entornos_o.simulador(SeisCuartos(),
+              AgenteAleatorio(('ir_Der', 'ir_Izq', 'subir', 'bajar', 'limpiar', 'nada')),
+             100)
+
+if __name__ == "__main__":
+    test()
+    e = SeisCuartos()
 # Requiere el modulo entornos_o.py
 # Usa el modulo doscuartos_o.py para reutilizar código
 # Agrega los modulos que requieras de python
