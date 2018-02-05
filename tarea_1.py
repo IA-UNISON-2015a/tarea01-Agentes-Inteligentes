69af95d939a5819b98ef803437fd6e8694945dec
Giovanni <giovanni.lopez3523@gmail.com>
2018-02-04 23:28:40 -0700

    Ultimo ejercicio agente estocastico

--- tarea_1.py
+++ tarea_1.py
@@ -61,6 +61,7 @@ __author__ = 'Giovanni_Lopez_Celaya'
 
 import entornos_o
 from random import choice
+import numpy.random as np
 
 class SeisCuartos(entornos_o.Entorno):
     """
@@ -217,7 +218,63 @@ class AgenteRacionalCiego(entornos_o.Agente):
                     self.acc='ir_A'
             
         return (self.acc)
-            
+
+class DosCuartosEstocastico(entornos_o.Entorno):
+    def __init__(self, x0=["A", "sucio", "sucio"]):
+        """
+        Por default inicialmente el robot está en A y los dos cuartos
+        están sucios
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        return acción in ("ir_A", "ir_B", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, a, b = self.x
+        if acción is not "nada" or a is "sucio" or b is "sucio":
+            self.desempeño -= 1
+        if acción is "limpiar":
+            self.x[" AB".find(self.x[0])] = "limpio"
+        elif acción is "ir_A":
+            self.x[0] = "A"
+        elif acción is "ir_B":
+            self.x[0] = "B"
+
+    def percepción(self):
+        return self.x[0], self.x[" AB".find(self.x[0])]
+
+class AgenteRacionalDosCuartosEstocastico(entornos_o.Agente):
+    """
+    Un agente reactivo basado en modelo
+    """
+    def __init__(self):
+        """
+        Inicializa el modelo interno en el peor de los casos
+        """
+        self.modelo = ['A', 'sucio', 'sucio']
+        
+    def programa(self, percepción):
+        robot, situación = percepción
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[' AB'.find(robot)] = situación
+
+        # Decide sobre el modelo interno
+        a, b = self.modelo[1], self.modelo[2]
+
+
+        if situación=='sucio':
+            return('limpiar' if np.randint(1, 100+1) <= 80 else 'nada')
+        else:
+            return('nada' if a == b == 'limpio' else
+                   'ir_A' if robot == 'B' else 'ir_B')
+
 def test():
     """
     Prueba del entorno y los agentes
@@ -233,6 +290,15 @@ def test():
     print("Prueba del entorno con un agente racional ciego")
     entornos_o.simulador(DosCuartosCiego(), AgenteRacionalCiego(), 100)    
     
+    print("Dos cuartos estocastico \n Prueba del entorno con un agente aleatorio")
+    entornos_o.simulador(DosCuartosEstocastico(),
+                        AgenteAleatorio(['ir_A', 'ir_B', 'limpiar', 'nada']),
+                        100)
+
+
+    print("Dos cuartos estocastico \n Prueba del entorno con un agente reactivo con modelo")
+    entornos_o.simulador(DosCuartosEstocastico(), AgenteRacionalDosCuartosEstocastico(), 100)
+
 if __name__ == "__main__":
     test()
     e = SeisCuartos()
