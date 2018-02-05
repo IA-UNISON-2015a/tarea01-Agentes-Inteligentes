c7afef1c9d51dc7d01a4e1f3f6fe0e375a1b9438
Giovanni <giovanni.lopez3523@gmail.com>
2018-02-04 23:18:30 -0700

    AgenteRacionalCiego se me olvido hacer el push en lo ultimo que hice

--- tarea_1.py
+++ tarea_1.py
@@ -162,6 +162,61 @@ class AgenteReactivoSeiscuartos(entornos_o.Agente):
         elif robot == 'F' and situación == 'limpio' or robot == 'E' and situación == 'limpio':
             return 'ir_Izq'
 
+class DosCuartosCiego(entornos_o.Entorno):
+    
+    def __init__(self, x0=["A", "sucio", "sucio"]):
+        """
+        Por default inicialmente el robot está en A y los dos cuartos
+        están sucios
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+        
+    def acción_legal(self, acción):
+        return acción in ("ir_A", "ir_B", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, a, b = self.x
+        
+        if acción is not "nada" or a is "sucio" or b is "sucio":
+            self.desempeño -= 1
+        if acción is "limpiar":
+            self.x[" AB".find(self.x[0])] = "limpio"
+        elif acción is "ir_A":
+            self.x[0] = "A"
+        elif acción is "ir_B":
+            self.x[0] = "B"
+            
+
+    def percepción(self):
+        return self.x[0]
+
+class AgenteRacionalCiego(entornos_o.Agente):
+    """
+    Un agente racional
+    """
+    def __init__(self):
+        self.acc_Ant=[]
+        self.acc=''
+    def programa(self, percepción):
+        robot = percepción
+        
+        if(len(self.acc_Ant)>=2):
+            self.acc='nada'
+        else:
+            if(not(robot in self.acc_Ant)):
+                self.acc_Ant.append(robot)
+                self.acc='limpiar'
+            else:
+                if(robot=='A'):
+                    self.acc='ir_B'
+                else:
+                    self.acc='ir_A'
+            
+        return (self.acc)
             
 def test():
     """
@@ -175,7 +230,8 @@ def test():
     print("Prueba del entorno con un agente reactivo")
     entornos_o.simulador(SeisCuartos(), AgenteReactivoSeiscuartos(), 100)
 
-    
+    print("Prueba del entorno con un agente racional ciego")
+    entornos_o.simulador(DosCuartosCiego(), AgenteRacionalCiego(), 100)    
     
 if __name__ == "__main__":
     test()
