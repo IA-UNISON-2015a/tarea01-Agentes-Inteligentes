c8195bffee27ba0590515bbcddf85babe15f63bd
Jhon Smith <jsmith@localhost.localdomain>
2018-02-05 17:31:44 -0700

    Adaptar clases de archivo doscuartos_o.py
    Las clases del archivo doscuartos_o.py fueron cambiadas para no tener
    que reescribirlas en el archivo tarea_1 y solamente tener que heredar
    de ellas.
    En general el nombre de los cuartos son 1 y 2 en lugar de A y B y
    las acciones son ir izquierda o derecha en lugar de ir a cuarto A o
    a cuarto B.

--- doscuartos_o.py
+++ doscuartos_o.py
@@ -20,20 +20,20 @@ class DosCuartos(entornos_o.Entorno):
     """
     Clase para un entorno de dos cuartos. Muy sencilla solo regrupa metodos.
 
-    El estado se define como (robot, A, B)
-    donde robot puede tener los valores "A", "B"
-    A y B pueden tener los valores "limpio", "sucio"
+    El estado se define como (robot, 1, 2)
+    donde robot puede tener los valores "1", "2"
+    1 y 2 pueden tener los valores "limpio", "sucio"
 
-    Las acciones validas en el entorno son ("ir_A", "ir_B", "limpiar", "nada").
+    Las acciones validas en el entorno son ("ir_Izquierda", "ir_Derecha", "limpiar", "nada").
     Todas las acciones son validas en todos los estados.
 
     Los sensores es una tupla (robot, limpio?)
     con la ubicacion del robot y el estado de limpieza
 
     """
-    def __init__(self, x0=["A", "sucio", "sucio"]):
+    def __init__(self, x0=[1, "sucio", "sucio"]):
         """
-        Por default inicialmente el robot esta en A y los dos cuartos
+        Por default inicialmente el robot esta en 1 y los dos cuartos
         estan sucios
 
         """
@@ -41,7 +41,7 @@ class DosCuartos(entornos_o.Entorno):
         self.desempenio = 0
 
     def accion_legal(self, accion):
-        return accion in ("ir_A", "ir_B", "limpiar", "nada")
+        return accion in ("ir_Izquierda", "ir_Derecha", "limpiar", "nada")
 
     def transicion(self, accion):
         if not self.accion_legal(accion):
@@ -51,14 +51,14 @@ class DosCuartos(entornos_o.Entorno):
         if accion is not "nada" or a is "sucio" or b is "sucio":
             self.desempenio -= 1
         if accion is "limpiar":
-            self.x[" AB".find(self.x[0])] = "limpio"
-        elif accion is "ir_A":
-            self.x[0] = "A"
-        elif accion is "ir_B":
-            self.x[0] = "B"
+            self.x[ self.x[0] ] = "limpio"
+        elif accion is "ir_Izquierda":
+            self.x[0] = 1
+        elif accion is "ir_Derecha":
+            self.x[0] = 2
 
     def percepcion(self):
-        return self.x[0], self.x[" AB".find(self.x[0])]
+        return self.x[0], self.x[ self.x[0] ]
 
 
 class AgenteAleatorio(entornos_o.Agente):
@@ -81,7 +81,7 @@ class AgenteReactivoDoscuartos(entornos_o.Agente):
     def programa(self, percepcion):
         robot, situacion = percepcion
         return ('limpiar' if situacion == 'sucio' else
-                'ir_A' if robot == 'B' else 'ir_B')
+                'ir_Izquierda' if robot == 2 else 'ir_Derecha')
 
 
 class AgenteReactivoModeloDosCuartos(entornos_o.Agente):
@@ -94,20 +94,20 @@ class AgenteReactivoModeloDosCuartos(entornos_o.Agente):
         Inicializa el modelo interno en el peor de los casos
 
         """
-        self.modelo = ['A', 'sucio', 'sucio']
+        self.modelo = [1, 'sucio', 'sucio']
 
     def programa(self, percepcion):
         robot, situacion = percepcion
 
         # Actualiza el modelo interno
         self.modelo[0] = robot
-        self.modelo[' AB'.find(robot)] = situacion
+        self.modelo[ self.modelo[0] ] = situacion
 
         # Decide sobre el modelo interno
         a, b = self.modelo[1], self.modelo[2]
         return ('nada' if a == b == 'limpio' else
                 'limpiar' if situacion == 'sucio' else
-                'ir_A' if robot == 'B' else 'ir_B')
+                'ir_Izquierda' if robot == 2 else 'ir_Derecha')
 
 
 def test():
@@ -117,7 +117,7 @@ def test():
     """
     print("Prueba del entorno con un agente aleatorio")
     entornos_o.simulador(DosCuartos(),
-                         AgenteAleatorio(['ir_A', 'ir_B', 'limpiar', 'nada']),
+                         AgenteAleatorio(['ir_Izquierda', 'ir_Derecha', 'limpiar', 'nada']),
                          100)
 
     print("Prueba del entorno con un agente reactivo")
