69c1e60908152dae9077f70e25b95c089573e621
unknown <saaigj@gmail.com>
2019-01-30 21:52:41 -0700

    entorno

--- tarea_1.py
+++ tarea_1.py
@@ -57,9 +57,134 @@ Todos los incisos tienen un valor de 25 puntos sobre la calificación de
 la tarea.
 
 """
-__author__ = 'escribe_tu_nombre'
+__author__ = 'Ariana Sánchez.'
 
-import entorno_o
+import entornos_o
+from random import choice
+
+class NueveCuartos(entornos_o.Entorno):
+     """
+    Clase para un entorno de dos cuartos. Muy sencilla solo regrupa métodos.
+
+    El estado se define como (robot, piso, A, B, C)
+    donde robot puede tener los valores "A", "B" o "C"
+    A, B y C pueden tener los valores "limpio", "sucio"
+
+    Las acciones válidas en el entorno son ("ir_izquierda", "ir_derecha", "subir", "bajar", "limpiar", "nada").
+    Las restricciones son:
+        Solo se puede subir de piso del lado derecho
+        Solo se puede bajar de piso del lado izquierdo
+        No se puede subir otro piso estando en el tercero
+        No se puede bajar otro piso estando en el primero
+
+    Los sensores es una tupla (robot, limpio?)
+    con la ubicación del robot y el estado de limpieza
+
+    """
+    def __init__(self, x0=["A", 1, ("sucio", "sucio", "sucio"), 
+                                   ("sucio", "sucio", "sucio"), 
+                                   ("sucio", "sucio", "sucio")]):
+        """
+        Por default inicialmente el robot está en A y todos los cuartos
+        están sucios
+
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        if self.x[1] == (1 and acción == "bajar") or (3 and acción == "subir"): return False
+        if self.x[0] == ("A" and acción == "ir_izquierda") or ("C" and acción == "ir_derecha"): return False
+        if self.x[0] != ("A" and acción == "bajar") or ("C" and acción == "subir"): return False
+        
+        return acción in ("ir_derecha", "ir_izquierda", "subir", "bajar", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, piso, piso_1, piso_2, piso_3 = self.x
+        if acción is not "nada" or piso_1 is "sucio" or piso_2 is "sucio" or piso_3 is "sucio":
+            self.desempeño -= 1
+        if acción is "limpiar":
+            self.x[" AB".find(self.x[0])] = "limpio"
+        elif acción is "ir_A":
+            self.x[0] = "A"
+        elif acción is "ir_B":
+            self.x[0] = "B"
+
+    def percepción(self):
+        return self.x[0], self.x[" AB".find(self.x[0])]
+
+
+class AgenteAleatorio(entornos_o.Agente):
+    """
+    Un agente que solo regresa una accion al azar entre las acciones legales
+
+    """
+    def __init__(self, acciones):
+        self.acciones = acciones
+
+    def programa(self, percepcion):
+        return choice(self.acciones)
+
+
+class AgenteReactivoDoscuartos(entornos_o.Agente):
+    """
+    Un agente reactivo simple
+
+    """
+    def programa(self, percepción):
+        robot, situación = percepción
+        return ('limpiar' if situación == 'sucio' else
+                'ir_A' if robot == 'B' else 'ir_B')
+
+
+class AgenteReactivoModeloDosCuartos(entornos_o.Agente):
+    """
+    Un agente reactivo basado en modelo
+
+    """
+    def __init__(self):
+        """
+        Inicializa el modelo interno en el peor de los casos
+
+        """
+        self.modelo = ['A', 'sucio', 'sucio']
+
+    def programa(self, percepción):
+        robot, situación = percepción
+
+        # Actualiza el modelo interno
+        self.modelo[0] = robot
+        self.modelo[' AB'.find(robot)] = situación
+
+        # Decide sobre el modelo interno
+        a, b = self.modelo[1], self.modelo[2]
+        return ('nada' if a == b == 'limpio' else
+                'limpiar' if situación == 'sucio' else
+                'ir_A' if robot == 'B' else 'ir_B')
+
+
+def test():
+    """
+    Prueba del entorno y los agentes
+
+    """
+    print("Prueba del entorno con un agente aleatorio")
+    entornos_o.simulador(NueveCuartos(),
+                         AgenteAleatorio(["ir_derecha", "ir_izquierda", "subir", "bajar", "limpiar", "nada"]),
+                         100)
+
+   # print("Prueba del entorno con un agente reactivo")
+   # entornos_o.simulador(DosCuartos(), AgenteReactivoDoscuartos(), 100)
+
+   # print("Prueba del entorno con un agente reactivo con modelo")
+   # entornos_o.simulador(DosCuartos(), AgenteReactivoModeloDosCuartos(), 100)
+
+
+if __name__ == "__main__":
+    test()
 
 # Requiere el modulo entornos_o.py
 # Usa el modulo doscuartos_o.py para reutilizar código
