c8e8d15ff7f08ad735527075f8200c3aec755dce
Unknown <josecarlos.pimienta@gmail.com>
2019-02-01 22:15:08 -0700

    nueveCuartosEstocasticos

--- tarea_1.py
+++ tarea_1.py
@@ -61,6 +61,7 @@ __author__ = 'Jose Carlos Pimienta Ibarra'
 
 import entornos_o
 from random import choice
+from random import randint
 
 # Requiere el modulo entornos_o.py
 # Usa el modulo doscuartos_o.py para reutilizar código
@@ -145,7 +146,242 @@ class NueveCuartos(entornos_o.Entorno):
 
     def percepción(self):
         return self.x[0], self.x[self.x[0]]
+#---------------------------------------------------------------------
+#========NueveCuartosCiego
+#---------------------------------------------------------------------
+class NueveCuartosCiego(NueveCuartos):
+    
+    def __init__(self, x0=[1]):
+        """
+        Por default inicialmente el robot está en A 
 
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        if self.x[0]==1:
+            return acción in ("der", "limpiar", "nada")
+        if self.x[0]==2:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==3:
+            return acción in ("izq", "subir" , "limpiar", "nada")
+        if self.x[0]==4:
+            return acción in ("der", "bajar", "limpiar", "nada")
+        if self.x[0]==5:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==6:
+            return acción in ("izq", "subir" , "limpiar", "nada")
+        if self.x[0]==7:
+            return acción in ("der", "bajar", "limpiar", "nada")
+        if self.x[0]==8:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==9:
+            return acción in ("izq", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            #raise ValueError("La acción no es legal para este estado")
+            acción = "nada"
+
+        robot, a, b, c, d, e, f, g, h, i = self.x
+        if acción is not "nada":
+            self.desempeño -= 1
+        
+        # limpiar, ir_Derecha e ir_izquierda cuestan 1
+        #subir y bajar cuestan 2
+        if acción is "limpiar":
+            self.desempeño-=1
+        elif acción is "der":
+            self.desempeño-=1
+            self.x[0] += 1 
+        elif acción is "izq":
+            self.desempeño-=1
+            self.x[0] -= 1 
+        elif acción is "subir":
+            self.desempeño-=2
+            self.x[0] += 3 
+        elif acción is "bajar":
+            self.desempeño-=2
+            self.x[0] -= 3
+
+    def percepción(self):
+        return self.x[0]
+    
+
+#---------------------------------------------------------------------
+#========NueveCuartosEstocastico
+#---------------------------------------------------------------------
+class NueveCuartosEstocastico(entornos_o.Entorno):
+    """
+    Esta clase es parecida a NueveCuarto
+    a diferencia que tiene la incertidumbre  de 
+    que a veces no seguira las acciones correctamente
+
+    """
+    def __init__(self, x0=[1, "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"]):
+        """
+        Por default inicialmente el robot está en A y los dos cuartos
+        están sucios
+
+        """
+        self.x = x0[:]
+        self.desempeño = 0
+
+    def acción_legal(self, acción):
+        if self.x[0]==1:
+            return acción in ("der", "limpiar", "nada")
+        if self.x[0]==2:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==3:
+            return acción in ("izq", "subir" , "limpiar", "nada")
+        if self.x[0]==4:
+            return acción in ("der", "bajar", "limpiar", "nada")
+        if self.x[0]==5:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==6:
+            return acción in ("izq", "subir" , "limpiar", "nada")
+        if self.x[0]==7:
+            return acción in ("der", "bajar", "limpiar", "nada")
+        if self.x[0]==8:
+            return acción in ("der","izq", "limpiar", "nada")
+        if self.x[0]==9:
+            return acción in ("izq", "limpiar", "nada")
+
+    def transición(self, acción):
+        if not self.acción_legal(acción):
+            #raise ValueError("La acción no es legal para este estado")
+            acción = "nada"
+
+        robot, a, b, c, d, e, f, g, h, i = self.x
+        if acción is not "nada" or "sucio" in {a,b,c,d,e,f,g,h,i}:
+            self.desempeño -= 1
+        
+        aleatorio = randint(0,9)
+        # limpiar, ir_Derecha e ir_izquierda cuestan 1
+        #subir cuesta 3 y bajar cuesta 2
+        if acción is "limpiar":
+            self.desempeño-=1
+            if aleatorio < 8:
+                self.x[self.x[0]] = "limpio"
+        elif acción is "der":
+            if aleatorio < 8:
+                self.desempeño-=1
+                self.x[0] += 1
+            elif aleatorio == 8:
+                self.desempeño-=1
+            else:##elejimos una accion aleatoriamente
+                ##vemos donde esta para escogfer su accion aleatoria
+                if robot == 1:
+                    self.desempeño-=1
+                    accion_aleatoria= choice(['limpiar','nada'])
+                    if accion_aleatoria == 'limpiar':
+                        self.x[robot]='limpio'
+                #-----------terminamos las acciones del cuarto 1
+                if robot == 4 or robot == 7:
+                    accion_aleatoria= choice(['limpiar','nada', 'bajar'])
+                    if accion_aleatoria == 'limpiar':
+                        self.desempeño-=1
+                        self.x[robot]='limpio'
+                    elif accion_aleatoria == 'bajar':
+                        self.desempeño-=2
+                        self.x[0] -= 3
+                    else:
+                        self.desempeño-=1
+                #-------------terminamos las acciones de los cuartos 4 y 7
+                if robot == 2 or robot == 5 or robot == 8:
+                    accion_aleatoria= choice(['limpiar','nada', 'izq'])
+                    if accion_aleatoria == 'limpiar':
+                        self.desempeño-=1
+                        self.x[robot]='limpio'
+                    elif accion_aleatoria == 'izq':
+                        self.desempeño-=1
+                        self.x[0] -= 1
+                    else:
+                        self.desempeño-=1
+                #---------terminamos las acciones de los cuartos 2, 5, 8
+        #=====terminamos todas las acciones que podrian pasar por ir a la derecha
+        elif acción is "izq":
+            if aleatorio < 8:
+                self.desempeño-=1
+                self.x[0] -= 1
+            elif aleatorio == 8:
+                self.desempeño-=1
+            else:##elejimos una accion aleatoriamente
+                ##vemos donde esta para escogfer su accion aleatoria
+                if robot == 9:
+                    self.desempeño-=1
+                    accion_aleatoria= choice(['limpiar', 'nada'])
+                    if accion_aleatoria == 'limpiar':
+                        self.x[robot]='limpio'
+                #-----------terminamos las acciones del cuarto 1
+                if robot == 3 or robot == 6:
+                    accion_aleatoria= choice(['limpiar', 'nada', 'subir'])
+                    if accion_aleatoria == 'limpiar':
+                        self.desempeño-=1
+                        self.x[robot]='limpio'
+                    elif accion_aleatoria == 'subir':
+                        self.desempeño-=2
+                        self.x[0] += 3
+                    else:
+                        self.desempeño-=1
+                #-------------terminamos las acciones de los cuartos 4 y 7
+                if robot == 2 or robot == 5 or robot == 8:
+                    accion_aleatoria= choice(['limpiar', 'nada', 'der'])
+                    if accion_aleatoria == 'limpiar':
+                        self.desempeño-=1
+                        self.x[robot]='limpio'
+                    elif accion_aleatoria == 'der':
+                        self.desempeño-=1
+                        self.x[0] += 1
+                    else:
+                        self.desempeño-=1
+                #---------terminamos las acciones de los cuartos 2, 5, 8
+        #=====terminamos todas las acciones que podrian pasar por ir a la IZQUIERDA
+        elif acción is "subir":
+            if aleatorio < 8:
+                self.desempeño-=2
+                self.x[0] += 3
+            elif aleatorio == 8:
+                self.desempeño-=1
+            else:##elejimos una accion aleatoriamente
+                ##vemos donde esta para escogfer su accion aleatoria
+                accion_aleatoria= choice(['limpiar', 'nada', 'izq'])
+                if accion_aleatoria == 'limpiar':
+                    self.desempeño-=1
+                    self.x[robot]='limpio'
+                elif accion_aleatoria == 'izq':
+                    self.desempeño-=1
+                    self.x[0] -= 1
+                else:
+                    self.desempeño-=1
+        #=====terminamos todas las acciones que podrian pasar por SUBIR
+        elif acción is "bajar":
+            if aleatorio < 8:
+                self.desempeño-=2
+                self.x[0] -= 3
+            elif aleatorio == 8:
+                self.desempeño-=1
+            else:##elejimos una accion aleatoriamente
+                ##vemos donde esta para escogfer su accion aleatoria
+                accion_aleatoria= choice(['limpiar', 'nada', 'der'])
+                if accion_aleatoria == 'limpiar':
+                    self.desempeño-=1
+                    self.x[robot]='limpio'
+                elif accion_aleatoria == 'der':
+                    self.desempeño-=1
+                    self.x[0] += 1
+                else:
+                    self.desempeño-=1
+        #=====terminamos todas las acciones que podrian pasar por BAJAR
+
+    def percepción(self):
+        return self.x[0], self.x[self.x[0]]
+
+#---------------------------------------------------------------------
+#=======FIN ENTORNOS==================================================
+#---------------------------------------------------------------------
+        
 class AgenteAleatorio(entornos_o.Agente):
     """
     Un agente que solo regresa una accion al azar entre las acciones legales
@@ -242,7 +478,7 @@ def test():
 
     """
     print("Prueba del entorno con un agente aleatorio")
-    entornos_o.simulador(NueveCuartos(),
+    entornos_o.simulador(NueveCuartosEstocastico(),
                          AgenteAleatorio(['der', 'izq', 'subir', 'bajar', 'limpiar', 'nada']),
                          200)
 
@@ -250,7 +486,7 @@ def test():
     #entornos_o.simulador(NueveCuartos(), AgenteReactivoNuevecuartos(), 100)
 
     print("Prueba del entorno con un agente reactivo con modelo")
-    entornos_o.simulador(NueveCuartos(), AgenteReactivoModeloNueveCuartos(), 200)
+    entornos_o.simulador(NueveCuartosEstocastico(), AgenteReactivoModeloNueveCuartos(), 200)
 
 
 if __name__ == "__main__":
