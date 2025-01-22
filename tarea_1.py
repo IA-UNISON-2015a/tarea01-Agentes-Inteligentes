b086eaf1a4b789087f42e37cdf77b9c8a0948e67
JesusF10 <jesusfl06oqk@gmail.com>
2025-01-21 23:50:50 -0700

    Se definio el inicializador del entorno de nueve cuartos

--- tarea_1.py
+++ tarea_1.py
@@ -2,17 +2,30 @@
 # -*- coding: utf-8 -*-
 """
 tarea_1.py
-------------
-
-Revisa el archivo README.md con las instrucciones de la tarea.
-
 """
-__author__ = 'escribe_tu_nombre'
+__author__ = 'Jesus Flores Lacarra'
 
-import entornos_f
 import entornos_o
+from random import choice
 
-# Requiere el modulo entornos_f.py o entornos_o.py
-# Usa el modulo doscuartos_f.py para reutilizar código
-# Agrega los modulos que requieras de python
+class NueveCuartos(entornos_o.Entorno):
+    """
+    Clase para un entorno de nueve cuartos.
 
+    El estado se define como (robot, 1A, 1B, 1C, 2A, 2B, 2C, 3A, 3B, 3C)
+    donde robot puede tener los valores "1A", "1B", "1C", "2A", "2B", "2C", "3A", "3B", "3C"
+    y pueden tener los valores "limpio", "sucio"
+
+    Las acciones válidas en el entorno son ["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"].
+    Solo algunas acciones son posibles en algunos estados, por ejemplo, no se puede subir si el robot está en 3A.
+    Solamente se sube por la derecha y se baja por la izquierda.
+
+    Los sensores es una tupla (robot, limpio?)
+    con la ubicación del robot y el estado de limpieza.
+
+    """
+    
+    def __init__(self, x0=["1A", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio"]):
+        super().__init__(x0)
+        
+    
