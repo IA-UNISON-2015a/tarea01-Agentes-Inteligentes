4ac804e9cd43972a3fd135219caec2aa7bdbe841
JesusF10 <jesusfl06oqk@gmail.com>
2025-01-22 20:05:28 -0700

    Se agrega verificacion de accion legal

--- tarea_1.py
+++ tarea_1.py
@@ -34,47 +34,77 @@ class NueveCuartos(entornos_o.Entorno):
         elif accion == "ir_Izquierda":
             return self.x[0][1] != "A"
         elif accion == "bajar":
-            return self.x[0][0] != "1"
+            return self.x[0][0] != "1" and self.x[0][1] == "A"
         elif accion == "subir":
-            return self.x[0][0] != "3"
+            return self.x[0][0] != "3" and self.x[0][1] == "C"
         return accion in ["limpiar", "nada"]
     
     def transicion(self, accion):
         if not self.acción_legal(accion):
+            print("accion ilegal")
             raise ValueError("La acción no es legal para este estado")
-
-        robot, cuartos = self.x[0], self.x[1:]
         
+        robot, cuartos = self.x[0], self.x[1:]
+        floor = int(robot[0])
+        column = robot[1]
+
         if accion == "nada" and "sucio" in cuartos:
             self.costo += 1
         elif accion == "limpiar":
             self.costo += 1
-            self.x[1 - (int(self.x[0][0]))*3 + "ABC".find(self.x[0][1])] = "limpio"
+            cuartos[(floor - 1) * 3 + "ABC".find(column)] = "limpio"
         elif accion == "ir_Derecha":
             self.costo += 2
-            self.x[0] = self.x[0][0] + "ABC"["ABC".find(self.x[0][1]) + 1]
+            self.x[0] = robot[0] + "ABC"["ABC".find(column) + 1]
         elif accion == "ir_Izquierda":
             self.costo += 2
-            self.x[0] = self.x[0][0] + "ABC"["ABC".find(self.x[0][1]) - 1]
+            self.x[0] = robot[0] + "ABC"["ABC".find(column) - 1]
         elif accion == "bajar":
             self.costo += 3
-            self.x[0] = str(int(self.x[0][0]) - 1) + self.x[0][1]
-        else:
+            self.x[0] = str(floor - 1) + column
+        else:  # accion == "subir"
             self.costo += 3
-            self.x[0] = str(int(self.x[0][0]) + 1) + self.x[0][1]
+            self.x[0] = str(floor + 1) + column
+
+        self.x = [self.x[0]] + cuartos
             
     def percepcion(self):
-        return self.x[0], self.x[1 - (int(self.x[0][0]))*3 + "ABC".find(self.x[0][1])]
+        #print(int(self.x[0][0]))
+        return self.x[0], self.x[(1 - int(self.x[0][0]))*3 + "ABC".find(self.x[0][1])]
+    
+    # Ejemplo:
+    # self.x = ["1A", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"]
+    # self.x[0] = "1A"
+    
+
+class AgenteAleatorio(entornos_o.Agente):
+    """
+    Un agente que solo regresa una accion al azar entre las acciones legales.
+
+    """
+    def __init__(self, acciones):
+        self.acciones = acciones
+
+    def programa(self, _):
+        accion = choice(self.acciones)
+        while not NueveCuartos().acción_legal(accion):
+            accion = choice(self.acciones)
+        return accion
 
 def test():
     """
     Prueba de entorno y los agentes
     """
     
-    x0 = ["1A", "sucio", "sucio", "", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"] # Estado inicial
+    x0 = ["1A", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"] # Estado inicial
+    acciones = ["ir_Derecha", "ir_Izquierda", "subir", "bajar", "limpiar", "nada"]
+    
     print("Prueba del entorno con un agente aleatorio")
     entornos_o.simulador(NueveCuartos(x0),
-                         AgenteAleatorio(['ir_A', 'ir_B', 'limpiar', 'nada']),
+                         AgenteAleatorio(acciones),
                          100)
+    
+if __name__ == "__main__":
+    test()
     
     
