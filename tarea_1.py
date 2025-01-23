42adc97c51e71b1b326bab9b6877eeb8ab5d0612
JesusF10 <jesusfl06oqk@gmail.com>
2025-01-22 17:40:47 -0700

    Funcion de transicion y percepcion, primera aproximacion

--- tarea_1.py
+++ tarea_1.py
@@ -25,7 +25,56 @@ class NueveCuartos(entornos_o.Entorno):
 
     """
     
-    def __init__(self, x0=["1A", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio", "Sucio"]):
+    def __init__(self, x0=["1A", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"]):
         super().__init__(x0)
         
+    def accion_legal(self, accion):
+        if accion == "ir_Derecha":
+            return self.x[0][1] != "C"
+        elif accion == "ir_Izquierda":
+            return self.x[0][1] != "A"
+        elif accion == "bajar":
+            return self.x[0][0] != "1"
+        elif accion == "subir":
+            return self.x[0][0] != "3"
+        return accion in ["limpiar", "nada"]
+    
+    def transicion(self, accion):
+        if not self.acción_legal(accion):
+            raise ValueError("La acción no es legal para este estado")
+
+        robot, cuartos = self.x[0], self.x[1:]
+        
+        if accion == "nada" and "sucio" in cuartos:
+            self.costo += 1
+        elif accion == "limpiar":
+            self.costo += 1
+            self.x[1 - (int(self.x[0][0]))*3 + "ABC".find(self.x[0][1])] = "limpio"
+        elif accion == "ir_Derecha":
+            self.costo += 2
+            self.x[0] = self.x[0][0] + "ABC"["ABC".find(self.x[0][1]) + 1]
+        elif accion == "ir_Izquierda":
+            self.costo += 2
+            self.x[0] = self.x[0][0] + "ABC"["ABC".find(self.x[0][1]) - 1]
+        elif accion == "bajar":
+            self.costo += 3
+            self.x[0] = str(int(self.x[0][0]) - 1) + self.x[0][1]
+        else:
+            self.costo += 3
+            self.x[0] = str(int(self.x[0][0]) + 1) + self.x[0][1]
+            
+    def percepcion(self):
+        return self.x[0], self.x[1 - (int(self.x[0][0]))*3 + "ABC".find(self.x[0][1])]
+
+def test():
+    """
+    Prueba de entorno y los agentes
+    """
+    
+    x0 = ["1A", "sucio", "sucio", "", "sucio", "sucio", "sucio", "sucio", "sucio", "sucio"] # Estado inicial
+    print("Prueba del entorno con un agente aleatorio")
+    entornos_o.simulador(NueveCuartos(x0),
+                         AgenteAleatorio(['ir_A', 'ir_B', 'limpiar', 'nada']),
+                         100)
+    
     
