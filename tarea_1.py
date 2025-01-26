7863bdefba03afdcea58db2748d157465c835c1a
RaulLopez3108 <raul_leonardo20@outlook.es>
2025-01-26 15:25:24 -0700

    Version final

--- tarea_1.py
+++ tarea_1.py
@@ -14,9 +14,8 @@ from random import choice
 import random
 
 class NueveCuartos(entornos_f.Entorno):
-    def accion_legal(self,_,accion):
-                       
-        #Restringir las acciones dependiendo del estado
+    def accion_legal(self,_,accion):                       
+        
         return accion in ("ir_Derecha","ir_Izquierda","Subir","Bajar", "Limpiar", "Nada")
     
     def transicion(self, estado, accion):
@@ -60,18 +59,50 @@ class NueveCuartosCiego(NueveCuartos):
         return super().accion_legal(_, accion)
     
     def transicion(self, estado, accion):
-        return super().transicion(estado, accion)
+       return super().transicion(estado,accion)
     
     def percepcion(self, estado):
         return super().percepcion(estado)    
 
 class NueveCuartosEstocastico(NueveCuartos):
+
     def accion_legal(self, estado, accion):
         return super().accion_legal(estado, accion)
 
-    def transicion(self, estado, accion):
-        return super().transicion(estado, accion)
-    
+    def transicion(self, estado, accion):       
+        robot,a,b,c,d,e,f,g,h,i = estado       
+        aleatorio = random.random()
+        if accion in ["ir_Derecha", "ir_Izquierda"]:
+            c_local = 1
+        elif accion in ["Limpiar","Nada"]:
+            c_local = 0
+        else:
+            c_local = 2          
+
+            if aleatorio < 0.8:
+                return ((estado, c_local) if a == "Nada" else
+                (("B",a,b,c,d,e,f,g,h,i),c_local) if robot == 'C' and accion == 'ir_Derecha' or robot == 'A' and accion ==  'ir_Izquierda' else
+                (("E",a,b,c,d,e,f,g,h,i),c_local) if robot == 'D' and accion == 'ir_Izquierda' or robot == 'F' and accion == 'ir_Derecha' else
+                (("H",a,b,c,d,e,f,g,h,i),c_local) if robot == 'G' and accion ==  'ir_Izquierda' or robot == 'I' and accion == 'ir_Derecha'else
+                (("C",a,b,c,d,e,f,g,h,i),c_local) if robot == 'F' and accion == 'Bajar' or robot  == 'B' and accion == 'ir_Izquierda'else
+                (("F",a,b,c,d,e,f,g,h,i),c_local) if robot == 'I' and accion == 'Bajar' or robot == 'E' and accion ==  'ir_Izquierda'else
+                (("D",a,b,c,d,e,f,g,h,i),c_local) if robot == 'A' and accion == 'Subir' or robot == 'E' and accion == 'ir_Derecha'else
+                (("G",a,b,c,d,e,f,g,h,i),c_local) if robot == 'D' and accion == 'Subir' or robot == 'H' and accion == 'ir_Derecha'else
+                (("I",a,b,c,d,e,f,g,h,i),c_local) if robot == 'H' and accion == 'ir_Izquierda' else
+                (("A",a,b,c,d,e,f,g,h,i),c_local) if robot == 'B' and accion == 'ir_Derecha'else
+                ((robot,"limpio",b,c,d,e,f,g,h,i),c_local)if robot == "A"  and accion == 'Limpiar' else
+                ((robot,a,"limpio",c,d,e,f,g,h,i),c_local)if robot == "B"  and accion == 'Limpiar' else
+                ((robot,a,b,"limpio",d,e,f,g,h,i),c_local)if robot == "C"  and accion == 'Limpiar' else
+                ((robot,a,b,c,"limpio",e,f,g,h,i),c_local)if robot == "D"  and accion == 'Limpiar' else
+                ((robot,a,b,c,d,"limpio",f,g,h,i),c_local)if robot == "E"  and accion == 'Limpiar' else
+                ((robot,a,b,c,d,e,"limpio",g,h,i),c_local)if robot == "F"  and accion == 'Limpiar' else
+                ((robot,a,b,c,d,e,f,"limpio",h,i),c_local)if robot == "G"  and accion == 'Limpiar' else
+                ((robot,a,b,c,d,e,f,g,"limpio",i),c_local)if robot == "H"  and accion == 'Limpiar' else
+                ((robot,a,b,c,d,e,f,g,h,"limpio"),c_local) and accion == 'Limpiar')
+            else: 
+                return 'Nada'
+        
+        
     def percepcion(self, estado):
         return super().percepcion(estado)
     
@@ -148,17 +179,15 @@ class AgenteEstocastico(entornos_f.Agente):
             if robot in 'A' and (b == c != 'limpio') or robot in 'B' and (c == 'sucio') or robot in 'D' and (e == f != 'limpio') or robot in 'E' and (f == 'sucio') or robot in 'G' and (h == i != 'limpio') or robot in 'H' and (i == 'sucio'):
                 return 'ir_Izquierda'      
             elif robot in 'A' and (b == c != 'sucio') and (d == e == f != 'limpio') or robot in 'D' and (e == f != 'sucio') and (g == h == i != 'limpio'):
-                return 'Subir'        
+                 return 'Subir'        
             if robot in 'C' and (a == b != 'limpio') or (a == b == 'limpio') or robot in 'B' and (a == 'sucio') or robot in 'F' and (e == d != 'limpio') or robot in 'E' and (d == 'sucio') or robot in 'I' and (h == g != 'limpio') or robot in 'H' and (g == 'sucio'): 
                 return 'ir_Derecha'
             if robot in 'I' and (h == g != 'sucio') and (d == e == f != 'limpio') or robot in 'F' and (e == d != 'sucio') and (a == b == c != 'limpio'):
                 return 'Bajar'
+        elif aleatorio <0.9:
+            return 'Nada'
         else:
-            if aleatorio < 0.9:
-                return 'Nada'
-            else:
-                return accion
-            
+            return accion    
 class AgenteReactivo(entornos_f.Agente):
         
     def __init__(self):
@@ -201,7 +230,7 @@ def prueba_agente(agente):
             NueveCuartos(),
             agente,
             ["A", "sucio", "sucio","sucio","sucio","sucio","sucio","sucio","sucio","sucio"],
-            200
+            100
         ),
         ["A", "sucio", "sucio","sucio","sucio","sucio","sucio","sucio","sucio","sucio"]
     )
@@ -212,7 +241,7 @@ def prueba_agente_ciego(agente):
             NueveCuartos(),
             agente,
             ["A","?","?","?","?","?","?","?","?","?"],
-            200
+            100
         ),
         ["A","?","?","?","?","?","?","?","?","?",]
     )
@@ -237,7 +266,7 @@ def test():
     print("Prueba del entorno ciego con un agente racional")
     prueba_agente_ciego(AgenteReactivo())
 
-    print("-------------------------------------ESTOCASTICO-------------------------------")
+    print("------------------------------------ENTORNO ESTOCASTICO--------------------------------------------")
 
     print("Prueba del entrono estocastico con un agente aleatorio")
     prueba_agente(AgenteAleatorio(['ir_Izquierda','ir_Derecha','Bajar','Subir','Limpiar','Nada']))
