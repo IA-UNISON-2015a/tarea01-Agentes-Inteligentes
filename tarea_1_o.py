eb7dd89302ced7f2d8e65f9890d70962bd36d449
Georgina Salcido <geo.sav707@gmail.com>
2025-01-25 17:20:20 -0700

    Correccion caso ciego

--- tarea_1_o.py
+++ tarea_1_o.py
@@ -28,7 +28,6 @@ class NueveCuartos(entornos_o.Entorno):
 
         piso, cuarto, estado = self.x
 
-        # VER COMO HACER MÁS BONITO Y OPTIMIZADO
         if accion == "limpiar":
             self.costo += 1
             estado[piso][cuarto] = "limpio"
@@ -39,7 +38,7 @@ class NueveCuartos(entornos_o.Entorno):
         elif accion == "ir_Izquierda":
             self.costo += 2
             if cuarto > 0:
-                self.x[1] += 1
+                self.x[1] -= 1
         elif accion == "subir":
             self.costo += 3
             if cuarto == 2 and piso < 2:
@@ -63,7 +62,7 @@ class AgenteAleatorio(entornos_o.Agente):
     def programa(self, _):
         return choice(self.acciones)
 
-"""
+# NO GUARDA LOS ESTADOS DE LOS DEMÁS CUARTOS, SOLO CONOCE EL ESTADO DEL CUARTO DONDE ESTÁ
 class AgenteReactivoNuevecuartos(entornos_o.Agente):
     def programa(self, percepcion):
         piso, cuarto, situacion = percepcion
@@ -73,7 +72,6 @@ class AgenteReactivoNuevecuartos(entornos_o.Agente):
                 'ir_Izquierda' if cuarto > 0 else
                 'bajar' if cuarto == 0 and piso > 0 else
                 'nada' )
-"""
     
 class AgenteReactivoModeloNueveCuartos(entornos_o.Agente):
     def __init__(self):
@@ -90,8 +88,10 @@ class AgenteReactivoModeloNueveCuartos(entornos_o.Agente):
                 'ir_Derecha' if cuarto < 2 and 'sucio' in self.modelo[2][piso][cuarto+1:] else
                 'subir' if cuarto == 2 and piso < 2 and 'sucio' in self.modelo[2][piso+1] else
                 'ir_Izquierda' if cuarto > 0 and 'sucio' in self.modelo[2][piso][:cuarto] else
-                'bajar' if cuarto == 0 and piso > 0 and 'sucio' in self.modelo[2][:piso] else
+                'bajar' if cuarto == 0 and piso > 0 else
                 'nada')
+    
+    """and 'sucio' in self.modelo[2][:piso]"""
  
 class NueveCuartosCiego(NueveCuartos):
     def percepcion(self):
@@ -106,14 +106,24 @@ class AgenteRacionalNueveCuartosCiego(entornos_o.Agente):
 
         self.modelo[0] = piso
         self.modelo[1] = cuarto
-        self.modelo[2][piso][cuarto] = "limpio"
+        #self.modelo[2][piso][cuarto] = "limpio"
 
-        return ('limpiar' if self.modelo[2][piso][cuarto] == "sucio" else
-                'ir_Derecha' if cuarto < 2 and 'sucio' in self.modelo[2][piso][cuarto+1:] else
-                'subir' if cuarto == 2 and piso < 2 and 'sucio' in self.modelo[2][piso+1] else
-                'ir_Izquierda' if cuarto > 0 and 'sucio' in self.modelo[2][piso][:cuarto] else
-                'bajar' if cuarto == 0 and piso > 0 and 'sucio' in self.modelo[2][:piso] else
-                'nada')
+        if self.modelo[2][piso][cuarto] == 'sucio':
+            self.modelo[2][piso][cuarto] = "limpio"
+            return 'limpiar'
+        if piso < 2 and cuarto == 2: # and any("sucio" in piso_cuartos for piso_cuartos in self.modelo[2][piso:]):
+            return 'subir'
+        elif piso > 0 and cuarto == 0:# and any("sucio" in piso_cuartos for piso_cuartos in self.modelo[2][:piso]):
+            return 'bajar'
+        if "sucio" in self.modelo[2][piso]:
+            if cuarto < 2 and any('sucio' in cuartos for cuartos in self.modelo[2][piso][cuarto:]):
+                return 'ir_Derecha'
+            elif cuarto > 0:
+                return 'ir_Izquierda'
+        return 'nada'
+    
+    """and 'sucio' in self.modelo[2][piso][cuarto+1:]"""
+    """and 'limpio' in self.modelo[2][piso][cuarto+1:]"""
     
 class NueveCuartosEstocastico(NueveCuartos):
     def transicion(self, accion):
@@ -143,15 +153,42 @@ class AgenteRacionalEstocastico(entornos_o.Agente):
         self.modelo = [0, 0, [["sucio"] * 3 for _ in range(3)]]
 
     def programa(self, percepcion):
-        piso, cuarto = percepcion
+        piso, cuarto, situacion = percepcion
 
         self.modelo[0] = piso
         self.modelo[1] = cuarto
-        self.modelo[2][piso][cuarto] = "limpio"
+        self.modelo[2][piso][cuarto] = situacion
 
-        return ('limpiar' if self.modelo[2][piso][cuarto] == 'sucio' else
+        return ('limpiar' if situacion == 'sucio' else
                 'ir_Derecha' if cuarto < 2 and 'sucio' in self.modelo[2][piso][cuarto+1:] else
                 'subir' if cuarto == 2 and piso < 2 and 'sucio' in self.modelo[2][piso+1] else
                 'ir_Izquierda' if cuarto > 0 and 'sucio' in self.modelo[2][piso][:cuarto] else
                 'bajar' if cuarto == 0 and piso > 0 and 'sucio' in self.modelo[2][:piso] else
                 'nada')
+    
+def test():
+    x0=[0, 0, [["sucio"] * 3 for _ in range(3)]]
+    """  
+    print("Prueba del entorno con un agente aleatorio")
+    entornos_o.simulador(NueveCuartos(x0),
+                         AgenteAleatorio(['ir_Derecha', 'ir_Izquierda', 'subir', 'bajar', 'limpiar', 'nada']),
+                         200)
+    
+    print("Prueba del entorno con un agente reactivo con modelo")
+    entornos_o.simulador(NueveCuartos(x0), 
+                         AgenteReactivoModeloNueveCuartos(), 
+                         200)
+    """
+    print("Prueba del entorno con un agente racional ciego")
+    entornos_o.simulador(NueveCuartosCiego(x0), 
+                         AgenteRacionalNueveCuartosCiego(), 
+                         200)
+    """ 
+    print("Prueba del entorno ciego con un agente racional estocastico")
+    entornos_o.simulador(NueveCuartosEstocastico(x0), 
+                         AgenteRacionalEstocastico(), 
+                         200)
+    """
+                         
+if __name__ == "__main__":
+    test()
